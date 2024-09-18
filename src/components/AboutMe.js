876793a2f91fa04a4f02a8d09596c16b814462c1
urie-w <urieasia@gmail.com>
2024-09-18 15:49:14 -0600

    file structure and started componets

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+// AboutMe.js
+import React from 'react';
+
+function AboutMe() {
+    return (
+        <section>
+            <h2>About Me</h2>
+            <p>This is the About Me section.</p>
+        </section>
+    );
+}
+
+export default AboutMe;
