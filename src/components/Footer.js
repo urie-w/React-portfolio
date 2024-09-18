c092375ec6644a531f76fe1e533659d669606aaa
urie-w <urieasia@gmail.com>
2024-09-18 17:55:38 -0600

    removed extra files

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <a href="https://github.com/urie-w">GitHub</a>
+                <a href="https://github.com/urie-w">LinkedIn</a>
+                <a href="https://github.com/urie-w">Stack Overflow</a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function Header() {
+    return (
+        <header>
+            <h1> Urie Williams</h1>
+            <Navigation />
+        </header>
+    )
+}
+
+export default Header;
