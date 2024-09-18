98c66783449d3b1cc7238fb04a634bcec114a86f
urie-w <urieasia@gmail.com>
2024-09-18 17:26:17 -0600

    Css started

---
+++ src/components/Resume.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <section>
+            <h2>Resume</h2>
+            <h3>Proficiencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+            </ul>
+        </section>
+    );
+}
+
+export default Resume;
